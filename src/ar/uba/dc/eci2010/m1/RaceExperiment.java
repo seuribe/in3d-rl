6a342a327615b158b8adc418b42b8d868c7de4a2
suribe <suribe@2788dae1-8a8d-9b6c-e6cd-08dfc599d49f>
2010-08-22 14:27:23 +0000

    Hice un refactoring del experimento para que corra con JUnit 4 y no andar pisando las pruebas de uno y del otro.

--- src/ar/uba/dc/eci2010/m1/RaceExperiment.java
+++ src/ar/uba/dc/eci2010/m1/RaceExperiment.java
@@ -1,44 +1,86 @@
 package ar.uba.dc.eci2010.m1;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.rlcommunity.rlglue.codec.AgentInterface;
+import org.rlcommunity.rlglue.codec.EnvironmentInterface;
 import org.rlcommunity.rlglue.codec.LocalGlue;
 import org.rlcommunity.rlglue.codec.RLGlue;
-
-import ar.uba.dc.eci2010.m1.agent.MDPRacingAgent;
 import ar.uba.dc.eci2010.m1.agent.ModelBasedRacingAgent;
-import ar.uba.dc.eci2010.m1.agent.RMaxRacingAgent;
 
 /**
  * 
  *
  */
 public class RaceExperiment {
-	public static void main(String[] args) {
-		RaceEnvironment env = new RaceEnvironment("data/minitrack.txt");
-//		SampleSarsaAgent agent = new SampleSarsaAgent();
-		ModelBasedRacingAgent agent = new ModelBasedRacingAgent(20);
-//		MDPRacingAgent agent = new MDPRacingAgent(env.getMDP());
-//		RMaxRacingAgent agent = new RMaxRacingAgent(RaceEnvironment.MAX_REWARD, 5, 12);
-		
-		LocalGlue localGlueImplementation=new LocalGlue(env, agent);
-		RLGlue.setGlue(localGlueImplementation);
-		
-		
+
+	private AgentInterface agent;
+	private EnvironmentInterface env;
+	private LocalGlue glue;
+
+	@Before
+	public void setupEnv() {
+		env = new RaceEnvironment("data/track.txt");
+	}
+
+	@Test
+	public void runModelBasedTest() {
+		agent = new ModelBasedRacingAgent(10);
+		glue = new LocalGlue(env, agent);
+		RLGlue.setGlue(glue);
+
 		RLGlue.RL_init();
-		System.out.println("running experiments");
-		for (int i = 0 ; i < 100 ; i++) {
-			RLGlue.RL_episode(100000);
-			RLGlue.RL_agent_message("reset-explore-exploit");
+		
+		// Learn model
+		System.out.println("learning model...");
+		roamWithNoLearning(10000);
+
+		// Learn V
+		System.out.println("running experiments...");
+		runEpisodes(100, 1000);
+
+		// Evaluate
+		System.out.println("Evaluating...");
+		evaluate();
+	}
+
+	private void runEpisodes(int n, int steps) {
+		for (int i = 0 ; i < n ; i++) {
+			RLGlue.RL_episode(steps);
 			System.out.println(RLGlue.RL_num_steps());
 		}
-		System.out.println();
+	}
+
+	private void roamWithNoLearning(int steps) {
+		RLGlue.RL_agent_message("freeze learning");
+		RLGlue.RL_episode(steps);
+		RLGlue.RL_agent_message("unfreeze learning");
+	}
+	
+	public void runModelFreeTest() {
+		agent = new SampleSarsaAgent();
+		glue = new LocalGlue(env, agent);
+		RLGlue.setGlue(glue);
+		
+		RLGlue.RL_init();
+		
+		// Learn V
+		System.out.println("running experiments...");
+		runEpisodes(100, 1000);
+
+		// Evaluate
 		System.out.println("Evaluating...");
+		evaluate();
+	}
+	
+	public void evaluate() {
 		RLGlue.RL_agent_message("freeze learning");
 		double sum = 0;
 		double sum_squares = 0;
 		final int N_EPISODES = 100;
 		for (int i = 0; i < N_EPISODES; i++) {
             RLGlue.RL_episode(1000);
-            double ret = /*RLGlue.RL_return()*/RLGlue.RL_num_steps();
+            double ret = RLGlue.RL_num_steps();
             sum += ret;
             sum_squares += ret * ret;
         }
@@ -46,9 +88,6 @@ public class RaceExperiment {
 		double mean = sum / (double)N_EPISODES;
 		double variance = (sum_squares - (double)N_EPISODES * mean * mean) / ((double)N_EPISODES - 1.0f);
 		System.out.println("mean: " + mean + ", variance: " + variance);
-
-//		RLGlue.RL_env_message("show moves");
-//		RLGlue.RL_episode(500);
 
 		RLGlue.RL_agent_message("unfreeze learning");
 	}
